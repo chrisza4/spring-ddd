32987cf9d53a326c906dfce044317c1b6b3a31da
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2023-08-26 16:29:48 +0700

    Refine service

--- src/main/java/com/dreamDealership/domain/carCollection/service/DropCar.java
+++ src/main/java/com/dreamDealership/domain/carCollection/service/DropCar.java
@@ -3,9 +3,8 @@ package com.dreamDealership.domain.carCollection.service;
 import com.dreamDealership.domain.carCollection.entity.CarCollection;
 import com.dreamDealership.domain.carCollection.entity.Stock;
 
-public class DropService {
-
-    public void dropCarAtWarehouse(CarCollection carCollection, Stock stock) {
+public class DropCar {
+    public void operate(CarCollection carCollection, Stock stock) {
         carCollection.carDroppedToWarehouse();
         stock.incrementStockCountBy(1);
     }
